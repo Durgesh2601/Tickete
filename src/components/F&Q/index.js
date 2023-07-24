376e665a4f8d7bf21611201ab082728d87af32aa
Durgesh2601 <dk829445@gmail.com>
2023-07-25 01:46:32 +0530

    help desk

--- src/components/F&Q/index.js
+++ src/components/F&Q/index.js
@@ -1,15 +1,28 @@
-import { Col, Collapse, Row } from "antd";
+import { Button, Col, Collapse, Row, Space } from "antd";
 import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
-import { RenderHeader } from "../CommonComponents";
+import { RenderHeader, Text } from "../CommonComponents";
 import { fAndQContents } from "../../constants/data";
+import ChatsIcon from "../../assets/ChatsCircle.svg";
 import "./index.css";
 
 const FrequentlyAskedQuestions = () => {
+  const customStyle = {
+    text: {
+      color: "#7f2d0f",
+    },
+    button: {
+      background: "#FFF7ED",
+      color: "#7f2d0f",
+      border: "1px solid #7f2d0f",
+      borderRadius: "0.5rem",
+    },
+  };
+
   return (
     <div className="help-container">
       <RenderHeader title="Frequently Asked Questions" />
-      <Row>
-        <Col span={16}>
+      <Row justify="space-between">
+        <Col span={15}>
           <Collapse
             className="custom-collapse"
             style={{ background: "white" }}
@@ -22,7 +35,26 @@ const FrequentlyAskedQuestions = () => {
             }
           />
         </Col>
-        <Col></Col>
+        <Col span={6}>
+          <Space direction="vertical" className="help-desk">
+            <Row justify="space-between">
+              <Col>
+                <Row>
+                  <Text strong style={{ ...customStyle["text"] }}>
+                    Need help?
+                  </Text>
+                </Row>
+                <Text style={{ ...customStyle["text"] }}>
+                  We're here to help, 24*7
+                </Text>
+              </Col>
+              <Col>
+                <img src={ChatsIcon} alt="chat-circle" />
+              </Col>
+            </Row>
+            <Button style={{ ...customStyle["button"] }}>Chat with us</Button>
+          </Space>
+        </Col>
       </Row>
     </div>
   );
